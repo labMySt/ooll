c1adcd36256108e8027aafc92de729387e574897
RAnastasiya <rizhko.anastasiya@gmail.com>
2018-03-12 02:57:38 +0200

    update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,70 @@
-import React, { Component } from 'react';
-import '../App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  state = {users: []};
+const BasicExample = () => (
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/topics">Topics</Link>
+                </li>
+            </ul>
 
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
+            <hr />
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
-      </div>
-    );
-  }
-}
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/topics" component={Topics} />
+        </div>
+    </Router>
+);
 
-export default App;
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+);
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+);
+
+const Topics = ({ match }) => (
+    <div>
+        <h2>Topics</h2>
+        <ul>
+            <li>
+                <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/components`}>Components</Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+            </li>
+        </ul>
+
+        <Route path={`${match.url}/:topicId`} component={Topic} />
+        <Route
+            exact
+            path={match.url}
+            render={() => <h3>Please select a topic.</h3>}
+        />
+    </div>
+);
+
+const Topic = ({ match }) => (
+    <div>
+        <h3>{match.params.topicId}</h3>
+    </div>
+);
+
+export default BasicExample;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+//import './LoadServiceWorker'
+//import "babel-polyfill";
+import React from "react";
+import ReactDOM from "react-dom";
+import BasicExample from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<BasicExample />, document.getElementById("app"));
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './components/App';
+// import registerServiceWorker from './registerServiceWorker';
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
