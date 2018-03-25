83ff17f4756aab5ede737586f5784f4382c3d1bc
labMySt <33463536+labMySt@users.noreply.github.com>
2018-03-25 11:10:16 +0300

    Redux is working

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,38 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter, Router } from 'react-router-dom'
-import App from './App';
-import routes from './routes.js';
+import ReactDOM  from 'react-dom'
+import { BrowserRouter, Router, Switch, Route } from 'react-router-dom'
+import Welcome from "./components/Welcome";
+import SignUp from "./components/SignUp";
+import Contacts from "./components/Contacts";
+import Templates from "./components/Templates";
+import Support from "./components/Support";
+import { Provider, connect } from 'react-redux';
+import {createStore} from "redux";
+import './styles/index.css';
+import reducer from './redusers/index'
 
-render(
-    <div>
-        <BrowserRouter routes={routes}>
-            <App/>
+const store = createStore(reducer);
+
+
+const mapStateToWelcomeProps = (state) => {
+  return {
+    authenticated: state.authenticated
+  }
+};
+
+const WrappedWelcome = connect(mapStateToWelcomeProps)(Welcome);
+
+ReactDOM.render(
+     <Provider store = {store}>
+        <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={WrappedWelcome}/>
+            <Route path='/templates' component={Templates}/>
+            <Route path='/contacts' component={Contacts}/>
+            <Route path='/help' component={Support}/>
+            <Route path='/sign_up' component={SignUp}/>
+        </Switch>
         </BrowserRouter>
-    </div>,
+    </Provider>,
     document.getElementById('app')
 );
